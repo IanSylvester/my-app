f4cfb2343f05ccc1466cb1818b988835f4b657be
ian <sylvester16@gmail.com>
2022-06-09 17:47:42 -0400

    made first component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from "react";
+import Todo from "./components/Todo";
 
 function App() {
   return (
     <div>
       <h1>My To-do's</h1>
-      <div className="card">
-        <h2>Title</h2>
-        <div className="actions">
-          <button className="btn">Delete</button>
-        </div>
-      </div>
+      <Todo />
+      <Todo />
+      <Todo />
     </div>
   );
 }
